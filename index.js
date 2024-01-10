df7aef70fc0d07e1dfa0938463dec4170553303e
Arvind Tiwari <tiwariav@gmail.com>
2024-01-10 14:25:35 +0530

    feat: add postcss-global-data

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const postcssGlobalImport = require("postcss-global-import");
 const postcssImport = require("postcss-import");
 const postcssPresetEnv = require("postcss-preset-env");
 const postcssMixin = require("postcss-mixins");
+const globalData = require("@csstools/postcss-global-data");
 
 const presetEnvOptions = {
   autoprefixer: {
@@ -18,15 +19,21 @@ const presetEnvOptions = {
   stage: 1,
 };
 
-const getConfig = (env = "production") => ({
-  plugins: [
-    postcssGlobalImport(),
-    postcssImport(),
-    postcssMixin(),
-    postcssPresetEnv({ ...presetEnvOptions, env }),
-    cssnano({ preset: "advanced" }),
-  ],
-  sourceMap: env === "development",
-});
+const getConfig = (env = "production", { globalDataOptions } = {}) => {
+  const config = {
+    plugins: [
+      postcssGlobalImport(),
+      postcssImport(),
+      postcssMixin(),
+      postcssPresetEnv({ ...presetEnvOptions, env }),
+      cssnano({ preset: "advanced" }),
+    ],
+    sourceMap: env === "development",
+  };
+  if (globalDataOptions) {
+    config.plugins.splice(0, 0, globalData(globalDataOptions));
+  }
+  return config;
+};
 
 module.exports = { getConfig, presetEnvOptions };
